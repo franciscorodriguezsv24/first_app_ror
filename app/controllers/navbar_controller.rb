800ec213186e7367bc2deeee1001bd379be41b57
franciscorodriguezsv24 <franciscorodriguezsv24@gmail.com>
2023-09-06 20:01:42 -0600

    rutas en ruby

---
+++ app/controllers/navbar_controller.rb
@@ -0,0 +1,4 @@
+class NavbarController < ApplicationController
+    def index
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
 
   get '/welcome', to: 'home#index'
+
+  get '/data', to: 'data#index'
+
+  get '/navbar', to: 'navbar#index'
 end
